310d88a9dcbf43c055595f3b501c63529e554dda
ReedNathan001 <92693641+ReedNathan001@users.noreply.github.com>
2023-11-30 12:12:19 -0600

    npoint.io
    no more mysql, deliverjs, expresses, GitHub

--- Navigation/Screens/LocateScreen.jsx
+++ Navigation/Screens/LocateScreen.jsx
@@ -164,7 +164,7 @@ import { useFocusEffect } from '@react-navigation/native';
   */
 
   useEffect(() => {
-    fetch('https://3b2e-47-184-111-152.ngrok-free.app/api/legos')
+    fetch('https://api.npoint.io/f7689e80de563c693342')
     .then(response => {
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
