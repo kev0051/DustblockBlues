8e11c3b61c1d90de995d6269fe6463acb6eb0dd3
ReedNathan001 <92693641+ReedNathan001@users.noreply.github.com>
2023-12-04 19:35:36 -0600

    Revert "npoint.io"
    This reverts commit 310d88a9dcbf43c055595f3b501c63529e554dda.

--- Navigation/Screens/HomeScreen.jsx
+++ Navigation/Screens/HomeScreen.jsx
@@ -29,7 +29,7 @@ function HomeScreen({navigation}){
   }, []);
   */
   useEffect(() => {
-    fetch('https://api.npoint.io/f7689e80de563c693342')
+    fetch('https://3b2e-47-184-111-152.ngrok-free.app/api/legos')
     .then(response => {
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
