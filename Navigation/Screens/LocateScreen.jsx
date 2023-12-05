d5ed00524df1ed724d50da96aa1c40d72b3928a3
ReedNathan001 <92693641+ReedNathan001@users.noreply.github.com>
2023-12-04 21:45:56 -0600

    Revert "Optimized navigation screens, added tunnel depend."
    This reverts commit 92c361b4e697c2cdd65d625a46af40c8d8d8f507.

--- Navigation/Screens/LocateScreen.jsx
+++ Navigation/Screens/LocateScreen.jsx
@@ -151,15 +151,8 @@ import { useFocusEffect } from '@react-navigation/native';
     const [partLocation, setPartLocation] = useState(false)
     
   
-    //const legos = require('../../assets/database.json')
-    const [legos, setLegos] = useState([]);
-
-  useEffect(() => {
-    fetch('https://raw.githubusercontent.com/ReedNathan001/DBBDatabase/main/database.json')
-      .then(response => response.json())
-      .then(data => setLegos(data))
-      .catch(error => console.error(error));
-  }, []);
+    const legos = require('../../assets/database.json')
+    
     //speech function
     const speakPrediction = () => {
       const textToSay = 'LEGO piece Predicted' + legoPrediction[0].PartName;
