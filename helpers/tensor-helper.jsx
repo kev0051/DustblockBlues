bef05138d20ca71de40c70f9af4b8e91b49e64d5
ReedNathan001 <92693641+ReedNathan001@users.noreply.github.com>
2023-12-04 19:36:48 -0600

    Revert "Now loads model externally using jsdeliver"
    This reverts commit 790f1da1353d0d8a80165f13c5d5c3744b621a9e.

--- helpers/tensor-helper.jsx
+++ helpers/tensor-helper.jsx
@@ -5,20 +5,14 @@ import {bundleResourceIO, decodeJpeg} from '@tensorflow/tfjs-react-native';
 import {Base64Binary} from '../utils/utils';
 const BITMAP_DIMENSION = 224;
 
-//const modelJson = require('../model/model.json');
-//const modelWeights = require('../model/weights.bin');
+const modelJson = require('../model/model.json');
+const modelWeights = require('../model/weights.bin');
 
-// Load the model from a URL instead
-const loadModel = async () => {
-  const model = await tf.loadLayersModel('https://cdn.jsdelivr.net/gh/ReedNathan001/DBBDatabase@main/model.json');
-  return model;
-};
 // 0: channel from JPEG-encoded image
 // 1: gray scale
 // 3: RGB image
 const TENSORFLOW_CHANNEL = 3;
-export const getModel = loadModel
-/*
+
 export const getModel = async () => {
   try {
     // wait until tensorflow is ready
@@ -29,7 +23,7 @@ export const getModel = async () => {
     console.log('Could not load model', error);
   }
 };
-*/
+
 export const convertBase64ToTensor = async (base64) => {
   try {
     const uIntArray = Base64Binary.decode(base64);
