cae310e9285ec3455a9cb2c3c9afbd94df74fd2d
ReedNathan001 <92693641+ReedNathan001@users.noreply.github.com>
2024-02-28 12:07:24 -0600

    reverted back to latest version w/ no 403 loc bug

---
+++ Navigation/Screens/HistoryScreen.jsx
@@ -0,0 +1,92 @@
+import { StyleSheet, Text, View, ScrollView, TouchableWithoutFeedback, Pressable } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { ListItem, Avatar } from "@rneui/themed";
+import { Divider } from "@react-native-material/core";
+import React, { useState, useEffect, useContext } from "react";
+import InformationModal from '../../components/Information';
+import SettingsModal from '../../components/SettingsPop';
+// for theming page: react useContext and below
+import themeContext from '../../config/themeContext';
+import Feather from 'react-native-vector-icons/Feather'; // Icon from https://github.com/oblador/react-native-vector-icons
+
+import { useFocusEffect } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+function HistoryScreen({navigation}){
+  // theme
+  const theme = useContext(themeContext);
+  const [legos, setLegos] = useState([]);
+  const [history, setHistory] = useState([]);
+  const [results, setResults] = useState([]);
+
+  useFocusEffect(
+    React.useCallback(() => {
+        AsyncStorage.getItem('LegoDB').then( response => {
+          if(response != null | undefined) {
+            setLegos(JSON.parse(response))
+        AsyncStorage.getItem('LegoHistory').then(
+            response => {
+                if(response != null | undefined ){ // if there is already history on the device
+                    setHistory(response);
+                    var partIDs = history.split(" ");
+                    var sortedLegoHist = partIDs.map(x => legos.find(item => item.PartID === x)); 
+                    setResults(sortedLegoHist);
+                    //AsyncStorage.clear(); // clears all stored values
+            }
+            else if(response == null){ // if there is no history on the device
+                setResults([]);
+                setHistory([]);
+            }
+        });
+      }
+      else{
+        // error getting the database
+      }
+        return() => {
+
+        };
+      });
+    }, [legos, history])
+);
+
+ return(
+    <View style={{backgroundColor: theme.background}}>
+    
+    
+    {/* <Text style={{fontSize:20,position:'absolute',left:-5,bottom:-3,textAlign:'center'}}>Menu</Text> */}
+    
+    <InformationModal></InformationModal>
+    <SettingsModal></SettingsModal>
+     
+      <ScrollView style={{position:'relative', marginBottom:90, backgroundColor: theme.background}}>
+
+      <Text style={{...styles.text, left:20,fontWeight:'bold', fontSize:30, color: theme.color}}>History</Text>
+      <Divider style={{ marginTop: 10,marginLeft:20,marginRight:20,}}/>
+      {/* iterate over the json file and print one by one */}
+      
+      {results.map(item => (
+          <ListItem key = {item.PartID} onPress={() => navigation.navigate('Lego',{ item:item})} containerStyle={{backgroundColor: theme.theme == "dark" ? "#000000" : theme.background}} bottomDivider>
+          <Avatar size={70} source={{ uri: item.ImageURL }} /> 
+          <ListItem.Content>
+            <ListItem.Title style={{color: theme.color}}>{item.PartName}</ListItem.Title>
+            <ListItem.Subtitle style={{color: theme.color}}>{'Category: ' + item.Category}</ListItem.Subtitle>
+            </ListItem.Content> 
+          </ListItem>
+      ))}
+
+      </ScrollView>
+      <StatusBar style="auto" />
+      </View>
+    );
+}
+
+const styles = StyleSheet.create({
+  text:{
+    position:'relative',
+    left:20, 
+    marginBottom:10, 
+    fontSize:15,
+  }
+});
+
+export default HistoryScreen
--- README.md
+++ README.md
@@ -1,12 +1,58 @@
-# DustblockBlues
+Comprehensive Document
 
-There are many small bricks used to build LEGO Mindstorms robots. If you can’t see them in
-the tray, it can take extra time to find them. The app should help the user locate the part they
-need. Possible solutions include, using sound to help the user find the brick when the phone is
-help above the tray or if they take a picture of the tray the phone gives the user the location (e.g.
-upper right corner of the tray). Needs to be IOS or multiplatform. Some work was done on this
-last year, including collecting a lot of LEGO data. It would be great to see tis project continue
-forward.
+A: Overview
+
+    -Problem Context:
+        The problem we are working towards is fixing the lack of availability of applications that are geared towards LEGO Mindstorm sets in finding 
+        bricks for individuals that are visually impaired, as well as easing and providing convenience for people who are interested in building these sets.
+    -Goals:
+        We are aiming to create an application to aid in finding a specific LEGO Mindstorm Block in a large pile, as well as identify a single specific piece. 
+        This would help streamline the building of Mindstorm Robots and make building projects with Mindstorm Blocks accessible to all. 
+
+B: Assumptions
+    - We assume the user  will have a working IOS(14.8+)/Android(8.1+) with the Expo Go application phone to test the program
+
+C: Dependencies
+    -To install all necessary dependencies, run the line `npm install` in the source code folder. To view a list of all included dependencies, read the package.json file.
+    -The only API call made is to roboflow model to locate legos on the screen and retrieve their position on the screen.
+
+D: Constraints
+    -Identification model is limited and works about 60% of the time, also has trouble with similar pieces
+    -Locating model works extremely well but only with 23 parts, we did not have enough time to implement all parts (parts that work: 4513174, 4666579,  4121715, 4206482, 4640536, 4514553, 4198367, 4173941, 4153707, 4552347, 4248204, 4211651, 4162857, 6271161, 4652235, 4535768, 4582792, 4540797, 6195314, 6271827, 6331428, 6313453   )
+    -App uses too much ram and gpu, needs to be made light
+    -App is too heavy since there is no database and all parts are located locally, there is no backend
+
+E: Description of Deployment Artifacts
+    - Everything needed to run this program is included in the source code
+
+F: Data Creation
+    - The parts are all located in the source code, thus there is no database needed to run the app. All lego part data is located in the database.json file.
+
+G: Admin Credentials
+    - N/A
+
+H: Deployment process
+    - Unzip the file 
+    - Open the file with your IDE 
+        - if your phone is not on the same internet connection as your computer you will need to use the command "expo start --tunnel"
+    - Open the expo go app on your phone
+    - Scan the QR code provided in the terminal
+
+# How to Deploy
+
+yarn install
+
+npx expo start
+
+Or if facing issues, try:
+
+yard add expo@^49.0.0
+
+npx expo-doctor
+
+npx expo install --check
+
+npx expo start
 
 # TIL
 nodemodules handled by package managers. 
