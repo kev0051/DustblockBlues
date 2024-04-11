45bea4be156d10506c0736307c2ced3216b22226
Modmaster2000 <JohnKempf@my.unt.edu>
2024-04-11 17:01:34 -0500

    LocateScreen with ultralytics api implemented

--- Navigation/Screens/LocateScreen.jsx
+++ Navigation/Screens/LocateScreen.jsx
@@ -1,8 +1,19 @@
-import React, {useState, useEffect, useRef, useContext } from 'react';
-import {TouchableOpacity,Image,View,StyleSheet,Dimensions,Pressable,Modal,Text,ActivityIndicator, Vibration} from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import {
+  Image,
+  View,
+  StyleSheet,
+  Dimensions,
+  Pressable,
+  Modal,
+  Text,
+  ActivityIndicator,
+  Vibration,
+  Platform,
+} from 'react-native';
 import * as MediaLibrary from 'expo-media-library';
-import {AutoFocus, Camera} from 'expo-camera';
-import axios from 'axios';
+import { AutoFocus, Camera } from 'expo-camera';
+import axios from 'axios'; // Import Axios
 import * as ImageManipulator from 'expo-image-manipulator';
 import { useFocusEffect } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -12,136 +23,35 @@ import * as Speech from 'expo-speech';
 import ttsContext from "../../config/ttsContext";
 import tts from '../../config/tts';
 
-  //camera screen function with navigation as argument
-  function LocateScreen({route,navigation}){
-    
-    const [showLocatePop, setShowLocatePop] = useState(false);
-    useEffect(() => {
-      // Show the CameraPop component when the CameraScreen component mounts
-      setShowLocatePop(true);
-    }, []);
-    useFocusEffect(
-      React.useCallback(() => {
-        // Useful for cleanup functions
-        return () => {
-          try{
-            navigation.setParams({partId: null});
-            
-          }
-          catch(e){
+function LocateScreen({ route, navigation }) {
+  const [showLocatePop, setShowLocatePop] = useState(false);
+  useEffect(() => {
+    setShowLocatePop(true);
+  }, []);
 
-          }
-          
-        };
-      }, []));
+  useFocusEffect(
+    React.useCallback(() => {
+      return () => {
+        try {
+          navigation.setParams({ partId: null });
+        } catch (e) {}
+      };
+    }, [])
+  );
 
+  const textureDims = Platform.OS === 'ios' ? { height: 1920, width: 1080 } : { height: 1200, width: 1600 };
 
+  const [hasCameraPermission, setHasCameraPermission] = useState();
+  const [hasMediaLibraryPermission, setHasMediaLibraryPermission] = useState();
+  let cameraRef = useRef();
 
-    const textureDims = Platform.OS === 'ios' ?
-  {
-    height: 1920,
-    width: 1080,
-  } :
-  {
-    height: 1200,
-    width: 1600,
-  };
+  const [legoPrediction, setLegoPrediction] = useState(null);
+  const [showPrediction, setShowPrediction] = useState(false);  
+  const [legoLocations, setLegoLocations] = useState([]);
+  const [partLocation, setPartLocation] = useState(false);
+  const [legos, setLegos] = useState([]);
 
-    //camera permissions
-    const [hasCameraPermission, setHasCameraPermission] = useState();
-    const [hasMediaLibraryPermission, setHasMediaLibraryPermission] = useState();
-  
-
-    let cameraRef = useRef();
-
-
-    let frame1 = 0;
-    const computeRecognitionEveryNFrames1 = 60;
-    function takePic(){
-      
-      const loop = async () => {
-        try{
-          if(frame1 % computeRecognitionEveryNFrames1 === 0){
-            let options = {
-              base64: true,
-            };
-            
-            
-            let image = await cameraRef.current.takePictureAsync(options);
-            
-            const actions = [
-              {
-                resize: {
-                  width: 400,
-                  height: 512,
-                },
-              },
-            ];
-            const saveOptions = {
-              format: ImageManipulator.SaveFormat.JPEG,
-              base64: true,
-            };
-            image =  await ImageManipulator.manipulateAsync(image.uri, actions, saveOptions);
-            // image = await loadImageBase64(image.uri);
-            // MediaLibrary.saveToLibraryAsync(image.uri)
-      
-            axios({
-                method: "POST",
-                url: "https://detect.roboflow.com/lego-detection-fsxai/8",
-                params: {
-                    api_key: "O6l7hNk0vu6OfoC85nD8"
-                },
-                data: image.base64,
-                headers: {
-                  "Content-Type": "application/x-www-form-urlencoded"
-              }
-        
-            })
-            .then(function(response) {
-              let res = []
-              try{
-                if (route.params.partId){
-                  // console.log("ran")
-
-                    for(var i = 0; i < response.data.predictions.length; i++ ){
-                      if(response.data.predictions[i].class === route.params.partId){
-                        res = [response.data.predictions[i]];
-                        setPartLocation(true)
-                        Vibration.vibrate();
-                      }
-                  }
-                }else{
-                  res = response.data.predictions
-                }
-              }
-              catch(e){
-                res = response.data.predictions
-              }finally{
-                setLegoLocations(res)
-              }
-                
-    
-            })
-            .catch(function(error) {
-                console.log(error.message);
-            });
-            requestAnimationFrame(loop);
-          
-        }
-      }
-      catch(error){
-        return
-      }
-      }
-      
-        loop()
-      
-    };
-
-
-
-
-    const tts = useContext(ttsContext);
+  const tts = useContext(ttsContext);
 
     const handleTextPress = (text) => {  
       if (tts.ttsChoice === "true") {
@@ -150,99 +60,156 @@ import tts from '../../config/tts';
     };
     const warningText = 'The prediction confidence is low. If this error persists please ensure viewing the LEGO from a top-down angle, in a well-lit area, on a uniform background, and without any obstructions.';
 
-
-    //unused maybe useful variables
-    // const [isProcessing, setIsProcessing] = useState(false);
-
-
-    //this is where the state of prediction is updated
-    const [legoPrediction, setLegoPrediction] = useState(null);
-  
-    //prediction state to show prediction modal
-    const [showPrediction, setShowPrediction] = useState(false) 
-
-    const [legoLocations, setLegoLocations] = useState([])
-
-    const [partLocation, setPartLocation] = useState(false)
-    
-  
-    //const legos = require('../../assets/database.json')
-    const [legos, setLegos] = useState([]);
-
-    /*
+  /*
+  // Use to debug updates to legoLocations
   useEffect(() => {
-    fetch('https://raw.githubusercontent.com/ReedNathan001/DBBDatabase/main/database.json')
-      .then(response => response.json())
-      .then(data => setLegos(data))
-      .catch(error => console.error(error));
-  }, []);
+    console.log("lego locations updated:", legoLocations);
+    legoLocations.map((prediction, index) => {
+      for (var i = 0; i < legos.length; i++) {
+        if (legos[i].PartID === prediction.name) {
+          //console.log(legos[i]);
+          console.log("Found Part:", legos[i].PartID);
+          setLegoPrediction([legos[i], prediction.confidence.toFixed(2) * 100 ])
+          console.log("width: ", (Dimensions.get('window').width) * prediction.width);
+          console.log("height: ", ((Dimensions.get('window').height-130))* prediction.height);
+          console.log("top: ", (prediction.ycenter) * (Dimensions.get('window').height-130)) + (((Dimensions.get('window').height-130))* prediction.height/2);
+          console.log("left: ", (prediction.xcenter) * Dimensions.get('window').width) + ((Dimensions.get('window').width) * prediction.width/2);
+        }
+      }
+      //setShowPrediction(true); //set flag to display prediction
+  });
+  }, [legoLocations, legos]);
+  
+  //Use to debug updates to legoPredictions
+  useEffect(() => {
+    console.log("lego prediction updated:", legoPrediction);
+    console.log("show prediction updated: ", showPrediction);
+  }, [legoPrediction, showPrediction]);
   */
 
+  let frame1 = 0;
+  const computeRecognitionEveryNFrames1 = 60;
+  function takePic() {
+    const loop = async () => {
+        if (frame1 % computeRecognitionEveryNFrames1 === 0) {
+          let options = {
+            base64: true,
+          };
+
+          let image = await cameraRef.current.takePictureAsync(options);
+
+          const actions = [
+            {
+              resize: {
+                width: 400,
+                height: 512,
+              },
+            },
+          ];
+          const saveOptions = {
+            format: ImageManipulator.SaveFormat.JPEG,
+            base64: true,
+          };
+          image = await ImageManipulator.manipulateAsync(image.uri, actions, saveOptions);
+
+          const formData = new FormData();
+          formData.append('size', 640);
+          formData.append('confidence', 0.25);
+          formData.append('iou', 0.45);
+          formData.append('image', {
+            uri: image.uri,
+            name: 'image.jpg',
+            type: 'image/jpg',
+          });
+
+          axios.post('https://api.ultralytics.com/v1/predict/fmiQDH8IwFsy2KZNqqZp', formData,  {
+            headers: {
+              'x-api-key': '216ef9d083377da32bc3bf704fdcf69f085b4fff42',
+              'Content-Type': 'multipart/form-data',
+            },
+          })
+            .then((response) => {
+              let res = [];
+              //console.log(responseJson.data);
+              res = response.data.data;
+
+              // function to sanatize output
+              const regex = /\((\d+)\)/; // Regular expression to match the number inside parentheses
+              for(var i = 0; i < res.length; i++){
+                const match = res[i].name.match(regex); // Extract the number using regex
+                //console.log(match[1]);
+                res[i].name = match[1];
+              }
+              //console.log("res: ", res);
+              try{
+                if (route.params.partId) {
+                  for (var i = 0; i < res.length; i++) {
+                    if (res[i].name === route.params.partId) {
+                      res = [{ ...response.data.data[i], confidence: response.data.data[i].confidence * 100 }];
+                      setPartLocation(true);
+                      Vibration.vibrate();
+                    }
+                  }
+                }
+              } catch (e) {
+              } finally {
+              setLegoLocations(res);
+              }
+
+            })
+            .catch((error) => {
+              console.error(error);
+            });
+        }
+        requestAnimationFrame(loop);
+      } 
+
+    loop();
+  };
+
   useEffect(() => {
-    AsyncStorage.getItem('LegoDB').then(
-      database => {
-        if(database === null){
+    AsyncStorage.getItem('LegoDB').then((database) => {
+      if (database === null) {
         fetch('https://api.npoint.io/f7689e80de563c693342')
-        .then(response => {
-          if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
-        })
-        .then(
-          data => { setLegos(data);
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+          })
+          .then((data) => {
+            setLegos(data);
             AsyncStorage.setItem('LegoDB', JSON.stringify(data));
-          }
-      
-        )
-        .catch(error => console.error(error));
-        }
-      else{
+          })
+          .catch((error) => console.error(error));
+      } else {
         setLegos(JSON.parse(database));
       }
-    })
+    });
   }, []);
-    //speech function
-    const speakPrediction = () => {
-      const textToSay = legoPrediction[0].PartName; //easier to hear like this
-      if (tts.ttsChoice === "true") {
-        Speech.speak(textToSay);
-      }
-    };
-  // const speakDismiss = () => {
-  //     const textToSay = 'Dismiss';
-  //     Speech.speak(textToSay);
-  //   };
+
+  const speakPrediction = () => {
+    const textToSay = legoPrediction[0].PartName; //easier to hear like this
+    if (tts.ttsChoice === "true") {
+      Speech.speak(textToSay);
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      const cameraPermission = await Camera.requestCameraPermissionsAsync();
+      const mediaLibraryPermission = await MediaLibrary.requestPermissionsAsync();
+      setHasCameraPermission(cameraPermission.status === 'granted');
+      setHasMediaLibraryPermission(mediaLibraryPermission.status === 'granted');
+    })();
+  }, []);
+
+  if (hasCameraPermission === undefined) {
+    return <Text>Requesting permissions...</Text>;
+  } else if (!hasCameraPermission) {
+    return <Text>Permission for camera not granted. Please change this in settings.</Text>;
+  }
   
-
-
-
-
-
-
-
-    useEffect(() => {
-        (async () => {
-          //checks permissions
-          const cameraPermission = await Camera.requestCameraPermissionsAsync();
-          const mediaLibraryPermission = await MediaLibrary.requestPermissionsAsync();
-          setHasCameraPermission(cameraPermission.status === "granted");
-          setHasMediaLibraryPermission(mediaLibraryPermission.status === "granted");
-          
-
-        })();
-      }, []);
-
-
-      if (hasCameraPermission === undefined) {
-        return <Text>Requesting permissions...</Text>
-      } else if (!hasCameraPermission) {
-        return <Text>Permission for camera not granted. Please change this in settings.</Text>
-      }
-
-
-
-
     return (
       <View style={styles.container}>
             {showLocatePop && <LocatePop isVisible={showLocatePop} onClose={() => setShowLocatePop(false)} />}
@@ -307,23 +274,22 @@ import tts from '../../config/tts';
           </View>
         </Modal>
 
+      <Camera style={styles.container} ref={cameraRef} onCameraReady={() => takePic()} />
 
-      <Camera style={styles.container} ref={cameraRef} onCameraReady={()=>takePic()}/>
-      
       {legoLocations.map((prediction, index) => (
         [
         <View  key = {index} onStartShouldSetResponder={() => { {
           for (var i = 0; i < legos.length; i++){
-            if (legos[i].PartID === prediction.class){
+            if (legos[i].PartID === prediction.name){
               setLegoPrediction([legos[i], prediction.confidence.toFixed(2) * 100 ])
             };
           }
           setShowPrediction(true);
         } }} style={[styles.box, {
-          width:  (Dimensions.get('window').width/400) * prediction.width, 
-          height: ((Dimensions.get('window').height-130)/512)* prediction.height,
-          top: ((prediction.y/512) * (Dimensions.get('window').height-130)) - (((Dimensions.get('window').height-130)/512)* prediction.height/2), 
-          left: ((prediction.x / 400) * Dimensions.get('window').width) -  ((Dimensions.get('window').width/400) * prediction.width/2),
+          width:  (Dimensions.get('window').width) * prediction.width, 
+          height: ((Dimensions.get('window').height)) * prediction.height,
+          bottom: ((prediction.ycenter) * (Dimensions.get('window').height) + 130), 
+          left: ((prediction.xcenter) * Dimensions.get('window').width),
           // left:  prediction.x
           // width: 428, 400, 415
           // height: 796, 512, 605  
@@ -332,17 +298,17 @@ import tts from '../../config/tts';
           }]}
           >
             
-            <Text style ={styles.predictionClass}>{prediction.class} ({prediction.confidence.toFixed(2) * 100}%)</Text>
+            <Text style ={styles.predictionName}>{prediction.name} ({prediction.confidence.toFixed(2) * 100}%)</Text>
           </View>,
           
         
           partLocation && 
             <View key = {index+1} style={ styles.partLocationContainer}>
                 <Text style={ [{fontSize: 30,left: '6%',color: "#ff0000",}]}>Part Located!</Text>
-                <Text style={ styles.partLocationText}>Width:{((Dimensions.get('window').width/400) * prediction.width).toFixed(0)}</Text>
-                <Text style={ styles.partLocationText}>Height:{(((Dimensions.get('window').height-130)/512)* prediction.height).toFixed(0)}</Text>
-                <Text style={ styles.partLocationText}>X:{(((prediction.y/512) * (Dimensions.get('window').height-130)) - (((Dimensions.get('window').height-130)/512)* prediction.height/2)).toFixed(0)}</Text>
-                <Text style={ styles.partLocationText}>Y:{(((prediction.x / 400) * Dimensions.get('window').width) -  ((Dimensions.get('window').width/400) * prediction.width/2)).toFixed(0)}</Text>
+                <Text style={ styles.partLocationText}>Width:{((Dimensions.get('window').width) * prediction.width).toFixed(0)}</Text>
+                <Text style={ styles.partLocationText}>Height:{(((Dimensions.get('window').height))* prediction.height).toFixed(0)}</Text>
+                <Text style={ styles.partLocationText}>Y:{((Dimensions.get('window').height) - (((prediction.ycenter) * (Dimensions.get('window').height)) + 130))}</Text>
+                <Text style={ styles.partLocationText}>X:{(((prediction.xcenter) * Dimensions.get('window').width))}</Text>
             </View>]
           
         
@@ -353,51 +319,40 @@ import tts from '../../config/tts';
   };
 
 
-
-
-
-//css themes for UI
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
     width: '100%',
     height: '100%',
   },
-  partLocationContainer:{
+  partLocationContainer: {
     position: 'absolute',
     left: Dimensions.get('screen').width / 2 - 90,
     bottom: 30,
-    backgroundColor: "white",
-    opacity: .5,
+    backgroundColor: 'white',
+    opacity: 0.5,
     zIndex: 100,
     height: 120,
     borderRadius: 10,
     width: 185,
-
-    // justifyContent: 'center',
-    margin: 'auto',
   },
-  partLocationText:{
+  partLocationText: {
     left: '30%',
-    color: "#ff0000",
+    color: '#ff0000',
   },
-
   camera: {
     flex: 1,
-    flexDirection: "column",
-    justifyContent: "space-between",
-    width: Dimensions.get("window").width,
-    height: Dimensions.get("window").height,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
-
   modal: {
     flex: 1,
     width: '100%',
     height: '100%',
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: 'rgba(0,0,0,0.6)'
-    
+    backgroundColor: 'rgba(0,0,0,0.6)',
   },
   modalContent: {
     alignItems: 'center',
@@ -429,18 +384,18 @@ const styles = StyleSheet.create({
   },
   box: {
     borderWidth: 2,
-    backgroundColor: "transparent",
+    backgroundColor: 'transparent',
     position: 'absolute',
     zIndex: 75,
-    borderColor: "#ff0000",
+    borderColor: '#ff0000',
   },
-  predictionClass:{
+  predictionName: {
     position: 'absolute',
     color: '#ff0000',
     top: -20,
     width: 1000,
-  }
- 
+  },
 });
 
 export default LocateScreen;
+
